ff82d75de3fb84a41ad6f45f28dcd54e9f73e7dd
MALIJEDDIVENKATESH <malijeddivenkatesh19062001@gmail.com>
2021-01-13 15:22:05 +0530

    added footer and ui changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var bgswitch=document.getElementsByClassName('bgswitch');
-console.log(bgswitch)
 
 
 function change()
@@ -9,13 +8,60 @@ function change()
     if(!body.classList.contains('dmode')){
 
         body.classList.add('dmode')
-    }
-    else{
-        body.classList.remove('dmode');
+        body.backgroundColor= '#2d3436';
+         body.style.backgroundImage= 'linear-gradient(315deg, #2d3436 0%, #000000 74%)';
+         
+        }
+        else{
+            body.classList.remove('dmode');
+            body.style.backgroundImage= '';
+
+        
+
+
     }
 
+
+    var section =document.getElementsByClassName('section');
+    Array.from(section).forEach((value,index)=>{
+        let color=value.style.backgroundColor;
+
+      
+
+     if(!value.classList.contains('dmode')){
+
+         value.classList.add('dmode')
+     }
+     else{
+         value.classList.remove('dmode');
+     }
+
+     
+    })
+    
+    var heading=document.getElementsByClassName('heading');
+    
+    Array.from(heading).forEach((value,index)=>{
+
+    
+    if(!value.classList.contains('dmode')){
+
+        value.classList.add('dmode')
+    }
+    else{
+        value.classList.remove('dmode');
+    }
+
+
+    })
+    
+    
+  
+
+    
 }
 
 function popup(){
     alert("Thanks for your response ");
+
   }
